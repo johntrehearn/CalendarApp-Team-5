3068c25ee4e7b32bfb54df49f9222e9f2ad63271
tdora28 <tokaidora88@gmail.com>
2024-03-28 16:20:38 +0200

    Add mock state for imitating log in and log out changes on the Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,44 +1,60 @@
 'use client';
 
 import Link from 'next/link';
+import { useState } from 'react';
 
 type Props = {};
 
 const MenuItems = () => {
-  // MenuItems will take a boolean depending on the user's logged in status.
-  // It will return the correct links to display in the Header component below.
-  // When authentication is ready, combine the logic below with the correct menu items in the return statement:
+  // !!! logIn and logOut functions are for testing purposes only!
+  // !!! After authentication is ready, clean up the mock code below:
+  // 1. logOut will be handled differently (in connection with the backend)
+  // 2. Delete the onClick from the Log In and Sign Up links! logIn will be handled on the /register and /login pages
+  // Login status will be stored in a global state to keep track easier.
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const logIn = () => {
+    setIsLoggedIn(true);
+  };
+  const logOut = () => {
+    setIsLoggedIn(false);
+  };
 
-  // {
-  //   isLoggedIn ? 'Items when user is logged IN' : 'Items when user is logged OUT';
-  // }
-
-  return (
-    <>
-      {/* Items when user is logged IN */}
-      <li>
-        <Link href="/calendars" className="btn btn-sm btn-ghost">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-          </svg>
-          My Calendars
-        </Link>
-      </li>
-      {/* Log Out button will go here! */}
-
-      {/* Items when user is logged OUT */}
-      <li>
-        <Link href="/register" className="btn btn-sm btn-warning">
-          Sign Up
-        </Link>
-      </li>
-      <li>
-        <Link href="/login" className="btn btn-sm btn-outline btn-warning">
-          Log In
-        </Link>
-      </li>
-    </>
-  );
+  if (isLoggedIn) {
+    //  Items when user is logged IN
+    return (
+      <>
+        <li>
+          <Link href="/calendars" className="btn btn-sm btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+            </svg>
+            My Calendars
+          </Link>
+        </li>
+        <li>
+          <button onClick={logOut} className="btn btn-sm btn-outline btn-warning">
+            Log Out
+          </button>
+        </li>
+      </>
+    );
+  } else {
+    // Items when user is logged OUT
+    return (
+      <>
+        <li>
+          <Link onClick={logIn} href="/register" className="btn btn-sm btn-warning">
+            Sign Up
+          </Link>
+        </li>
+        <li>
+          <Link onClick={logIn} href="/login" className="btn btn-sm btn-outline btn-warning">
+            Log In
+          </Link>
+        </li>
+      </>
+    );
+  }
 };
 
 const Header = (props: Props) => {
@@ -56,14 +72,14 @@ const Header = (props: Props) => {
         {/* NAVBAR RIGHT SIDE */}
         <div className="navbar-end">
           {/* Menu for larger screens */}
-          <ul className="menu menu-horizontal px-1 hidden md:flex gap-5">
+          <ul className="menu menu-horizontal px-1 hidden sm:flex gap-5">
             <MenuItems />
           </ul>
 
           {/* Dropdown menu for small screens */}
-          <div className="dropdown relative md:hidden">
+          <div className="dropdown relative sm:hidden">
             {/* Hamburger icon */}
-            <button className="btn btn-ghost md:hidden">
+            <button className="btn btn-ghost sm:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
               </svg>
