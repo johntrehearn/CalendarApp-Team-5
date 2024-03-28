0faad3c06b52d133e7d3a2140fd14f9a5e6910ea
tdora28 <tokaidora88@gmail.com>
2024-03-28 09:40:08 +0200

    Add basic pages for routing

---
+++ frontend/src/app/calendars/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const SingleCalendarPage = (props: Props) => {
+  return <div>SingleCalendarPage goes here</div>;
+};
+
+export default SingleCalendarPage;
---
+++ frontend/src/app/calendars/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const CalendarsPage = (props: Props) => {
+  return <div>CalendarsPage goes here</div>;
+};
+
+export default CalendarsPage;
