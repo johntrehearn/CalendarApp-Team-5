012f8b43a54fd41080ad686dbc05fcfae91bd829
Dóra Tokai <tokaidora88@gmail.com>
2024-04-02 17:18:46 +0300

    Redirect logged out user from Calendars page

--- frontend/src/app/calendars/[id]/page.tsx
+++ frontend/src/app/calendars/[id]/page.tsx
@@ -1,8 +1,30 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuthContext } from '@/contexts/AuthContext';
+
 import React from 'react';
 
 type Props = {};
 
 const SingleCalendarPage = (props: Props) => {
+  const router = useRouter();
+  const { isLoggedIn } = useAuthContext();
+
+  // Redirect to the homepage if the user is not logged in
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.replace('/');
+    }
+  }, [isLoggedIn, router]);
+
+  // Render nothing if the user is not logged in
+  if (!isLoggedIn) {
+    return null;
+  }
+
+  // Render the content of page if the user is logged in
   return <div>SingleCalendarPage goes here</div>;
 };
 
--- frontend/src/app/calendars/page.tsx
+++ frontend/src/app/calendars/page.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuthContext } from '@/contexts/AuthContext';
 
 type Props = {};
 
 const CalendarsPage = (props: Props) => {
+  const router = useRouter();
+  const { isLoggedIn } = useAuthContext();
+
+  // Redirect to the homepage if the user is not logged in
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.replace('/');
+    }
+  }, [isLoggedIn, router]);
+
+  // Render nothing if the user is not logged in
+  if (!isLoggedIn) {
+    return null;
+  }
+
+  // Render the content of page if the user is logged in
   return <div>CalendarsPage goes here</div>;
 };
 
