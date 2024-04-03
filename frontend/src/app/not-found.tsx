1043d5c9ed42ff7eec3253e8fcd19c831fd1eb0c
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-04-03 09:03:41 +0300

    styling is done

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -3,12 +3,33 @@ import Image from "next/image";
 function PageNotFound() {
   return (
     <main>
-      <h1>There was the problem</h1>
-      <p>We could not find the page you are looking for.</p>
-      <p>
-        Go to <Link href="/"> HomePage</Link>
-      </p>
-      <Image src="/image.jpg" alt="Image" width={500} height={500} />
+      <div class="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="card lg:card-side bg-base-100 shadow-xl ">
+            <div className="card-body">
+              <h2 className="card-title">There was the problem!</h2>
+              <p>We could not find the page you are looking for.</p>
+              <div className="card-actions justify-end">
+                <p>
+                  Go to{" "}
+                  <Link href="/">
+                    {" "}
+                    <button className="btn btn-primary">HomePage</button>
+                  </Link>
+                </p>
+                <figure className="my-4 mx-4">
+                  <Image
+                    src="/image.jpg"
+                    alt="Image"
+                    width={400}
+                    height={400}
+                  />
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
