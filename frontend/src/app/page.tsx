f62ce975ae89e22b15dbaec985185049a89b5db9
tdora28 <tokaidora88@gmail.com>
2024-04-08 13:26:59 +0300

    Add utility classes for custom colours

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import { fontTitle } from './layout';
 export default function Home() {
   return (
     <main>
-      <h1 className={`${fontTitle} title`}>Personalized Online Advent Calendars</h1>
+      <h1 className={`${fontTitle} clr-accent text-4xl`}>Personalized Online Advent Calendars</h1>
     </main>
   );
 }
