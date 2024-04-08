6dbfe4e09e45911ccf2faa5e756c6dd5915adf6e
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-04-08 17:20:50 +0300

    added background color according totheme

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -8,25 +8,30 @@ interface Props {}
 const PageNotFound: React.FC<Props> = (): ReactElement => {
   return (
     <main>
-      <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
+      <div className="relative isolate overflow-hidden bg-[#131625] py-16 sm:py-24 lg:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="card lg:card-side bg-base-100 shadow-xl ">
-            <div className="card-body">
-              <h2 className="card-title">There was the problem!</h2>
-              <p>We could not find the page you are looking for.</p>
+          <div className="card lg:card-side shadow-xl  ">
+            <div className="card-body text-[#EFD7BB]">
+              <h2 className="card-title w-96 text-3xl">
+                Lost in the North Pole: 404 - Page Not Found
+              </h2>
+              <p className="w-80">
+                Looks like our webpage took a detour through Santa's workshop
+                and got lost in the tinsel.
+              </p>
               <div className="card-actions justify-end">
                 <p>
                   <Link href="/">
-                    <button className="btn btn-primary">HomePage</button>
+                    <button className="btn bg-[#EFD7BB]">Go To Home</button>
                   </Link>
                 </p>
                 <figure className="my-4 mx-4">
-                  <Image
+                  {/*  <Image
                     src="/image.jpg"
                     alt="Image"
                     width={400}
                     height={400}
-                  />
+                  /> */}
                 </figure>
               </div>
             </div>
