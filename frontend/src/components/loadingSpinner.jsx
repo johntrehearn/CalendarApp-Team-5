0d3782adea7ecf34b970d6cacd9ca0b43fdf0050
johntrehearn <johntrehearnbc@gmail.com>
2024-04-10 15:52:19 +0300

    test loading spinner

---
+++ frontend/src/components/loadingSpinner.jsx
@@ -0,0 +1,12 @@
+
+
+const Spinner = () => {
+    return (
+        <div className="h-screen flex items-center justify-center">
+
+            <span className="loading loading-spinner text-secondary w-80 shadow-xl d-flex justify-content-center align-items-center " ></span>
+        </div >
+    );
+};
+
+export default Spinner;
