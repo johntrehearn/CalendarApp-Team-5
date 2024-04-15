e1d36a7c46d1bce12d39e65343d4ffc34bb5d221
Dóra Tokai <tokaidora88@gmail.com>
2024-04-15 15:26:48 +0300

    Add basic structure for the Calendar component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import { fontTitle } from './layout';
+import Calendar from '@/components/Calendar';
 
 export default function Home() {
   return (
     <main>
       <h1 className={`${fontTitle} clr-accent text-4xl`}>Personalized Online Advent Calendars</h1>
+      <Calendar />
     </main>
   );
 }
--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+'use client';
 
-type Props = {};
+const Calendar: React.FC = () => {
+  // Backgrounds to test:
+  // https://images.pexels.com/photos/66284/winter-nature-season-trees-66284.jpeg
+  // https://images.pexels.com/photos/1028723/pexels-photo-1028723.jpeg
+  // https://images.pexels.com/photos/18512842/pexels-photo-18512842/free-photo-of-autumn-forest-at-night.jpeg
 
-const Calendar = (props: Props) => {
-  return <div>Calendar</div>;
+  return (
+    <div>
+      <div className="p-10 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url(https://images.pexels.com/photos/18512842/pexels-photo-18512842/free-photo-of-autumn-forest-at-night.jpeg)' }}>
+        <p className="w-max text-center text-3xl m-5 bg-[white] py-3 px-10 rounded-lg mx-auto mb-10">Advent Calendar</p>
+        <div className="flex gap-5 flex-wrap justify-center"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Calendar;
