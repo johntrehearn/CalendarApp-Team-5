1c38244eb237432e43769eec8581f27accd350de
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-04-16 12:39:34 +0300

    404 content is centered

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 const PageNotFound: React.FC = () => {
   return (
     <main>
-      <div className="relative isolate overflow-hidden  py-16 sm:py-24 lg:py-32">
+      <div className=" flex justify-center items-center h-screen py-16 sm:py-24 lg:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="card lg:card-side shadow-xl  ">
-            <div className="card-body text-[#EFD7BB]">
+            <div className="card-body clr-accent">
               <h2 className="card-title w-96 text-3xl">
                 Lost in the North Pole: 404 - Page Not Found
               </h2>
@@ -17,7 +17,7 @@ const PageNotFound: React.FC = () => {
               <div className="card-actions justify-end">
                 <p>
                   <Link href="/">
-                    <button className="btn bg-[#EFD7BB]">Go To Home</button>
+                    <button className="btn bg-accent">Go To Home</button>
                   </Link>
                 </p>
               </div>
