6197a5eeea50a10e8d69bccbddf4d995297101aa
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-04-16 13:04:17 +0300

    fix the button position

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -14,7 +14,7 @@ const PageNotFound: React.FC = () => {
                 Looks like our webpage took a detour through Santa's workshop
                 and got lost in the tinsel.
               </p>
-              <div className="card-actions justify-end">
+              <div className="card-actions  flex justify-center">
                 <p>
                   <Link href="/">
                     <button className="btn bg-accent">Go To Home</button>
