f51cc520921ff8d38766f5f0effb76e8c3b12d37
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-04-17 10:13:19 +0300

    this change in text

--- frontend/src/app/decline/page.tsx
+++ frontend/src/app/decline/page.tsx
@@ -23,7 +23,7 @@ function PaymentDecline() {
                   d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-              <span>Oh NO! Task failed </span>
+              <span>Oh NO! Payment failed </span>
             </div>
             <div className="card-actions justify-end">
               <button className="btn bg-accent ">Try Again</button>
