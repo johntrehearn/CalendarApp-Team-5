072ae24d07b5d7c64afba9cce8624b501815ddcd
Dóra Tokai <tokaidora88@gmail.com>
2024-04-18 09:51:15 +0300

    Set dark bg behind text

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="content-width grid gap-10 md:grid-cols-[2fr_1fr] py-20">
-      <div className="flex flex-col justify-center items-center md:items-start gap-5">
+      <div className="flex flex-col justify-center items-center md:items-start gap-5 bg-base">
         <h1 className={`${fontTitle} clr-accent text-4xl text-center md:text-left`}>Personalized Online Advent Calendars</h1>
         <p className="clr-accent">
           Create a delightful online advent calendar filled with your cherished images and heartwarming messages. Embrace the joy of the season by personalizing each day's surprise. Our user-friendly platform allows you to craft a unique countdown
