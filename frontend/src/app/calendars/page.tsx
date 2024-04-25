94b0f0e7f5e5aae99ffb0f9ef3bc5e914172afd7
Dóra Tokai <tokaidora88@gmail.com>
2024-04-25 16:21:50 +0300

    Add delete modal, dummy data, and other card btn functionalities

--- frontend/src/app/calendars/page.tsx
+++ frontend/src/app/calendars/page.tsx
@@ -12,53 +12,112 @@ const CalendarsPage = () => {
   const router = useRouter();
   const { isLoggedIn } = useAuthContext();
 
+  // Redirect to the homepage if the user is not logged in
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.replace('/');
+    }
+  }, [isLoggedIn, router]);
+
+  // Hard coded calendars data for now
   const calendars = [
     {
+      id: '1234',
       title: 'Pet Calendar',
-      imageUrl: 'https://images.pexels.com/photos/357141/pexels-photo-357141.jpeg',
+      backgroundUrl: 'https://images.pexels.com/photos/357141/pexels-photo-357141.jpeg',
     },
     {
+      id: '5678',
       title: 'Your Advent Calendar',
-      imageUrl: 'http://source.unsplash.com/a-close-up-of-a-very-large-star-in-the-sky-HvYO12QeS5g?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+      backgroundUrl: 'http://source.unsplash.com/a-close-up-of-a-very-large-star-in-the-sky-HvYO12QeS5g?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
     },
   ];
 
-  // Redirect to the homepage if the user is not logged in
-  // useEffect(() => {
-  //   if (!isLoggedIn) {
-  //     router.replace('/');
-  //   }
-  // }, [isLoggedIn, router]);
+  // State for the incoming calendars data
+  const [calendarsData, setCalendarsData] = useState<
+    | {
+        id: string;
+        title: string;
+        backgroundUrl: string;
+      }[]
+    | null
+  >(calendars);
 
-  // Render nothing if the user is not logged in
-  // if (!isLoggedIn) {
-  //   return null;
-  // }
-
+  // State to store the selected card
   const [selectedCard, setSelectedCard] = useState('');
 
-  // Render the content of page if the user is logged in
+  // When a card button is clicked, handleAction sets the selected card and performs the chosen action
+  const handleAction = (id: string, action: string) => {
+    // Setting the selected card
+    setSelectedCard(id);
+
+    // Navigate to the single calendar page and sending the cal id
+    if (action === 'show') {
+      router.push(`/calendar/${id}`);
+    }
+
+    // Open the delete modal for more actions
+    if (action === 'delete') {
+      openDeleteModal();
+    }
+
+    // Navigate to the edit page - also needs id???
+    if (action === 'edit') {
+      router.push('/edit');
+    }
+
+    // For now, just log which calendar is being shared
+    if (action === 'share') {
+      console.log(`Sharing calendar with id: ${id}`);
+    }
+  };
+
+  const openDeleteModal = () => {
+    const modal = document.getElementById('delete_modal') as HTMLDialogElement;
+    if (modal) {
+      modal.showModal();
+    }
+  };
+
+  const deleteCalendar = () => {
+    console.log(`Deleting calendar with id: ${selectedCard}`);
+  };
+
+  // Get the title of the selected calendar for the delete modal
+  const getSelectedCalTitle = () => {
+    const selectedCal = calendarsData?.find((cal) => cal.id === selectedCard);
+    return selectedCal?.title;
+  };
+
+  // Render nothing if the user is not logged in
+  if (!isLoggedIn) {
+    return null;
+  }
+
+  // Render the content of the page if the user is logged in
   return (
     <main className="content-width flex flex-col items-center gap-10">
       <div className={`${fontTitle} clr-accent text-4xl text-center bg-base`}>Welcome, Bartholomew!</div>
 
-      {/* Alert */}
-      {!calendars && (
+      {/* Info when there's no calendar to show */}
+      {!calendarsData && (
         <div role="alert" className="alert mx-auto">
           <BsInfoCircle className="text-primary text-xl" />
           <span>You don't have any calendars yet.</span>
           <div>
-            <button className="btn btn-sm btn-primary">Create</button>
+            <Link href="/new" className="btn btn-sm btn-primary">
+              Create
+            </Link>
           </div>
         </div>
       )}
 
-      {/* Cards */}
-      {calendars && (
+      {/* Cards with New Calendar btn */}
+      {calendarsData && (
         <>
           <div className="flex flex-wrap gap-5 justify-center">
-            {calendars.map((calendar) => (
-              <CalendarCard key={calendar.title} title={calendar.title} imageUrl={calendar.imageUrl} />
+            {calendarsData.map((calendar) => (
+              <CalendarCard key={calendar.id} id={calendar.id} title={calendar.title} imageUrl={calendar.backgroundUrl} handleAction={handleAction} />
             ))}
           </div>
 
@@ -67,6 +126,22 @@ const CalendarsPage = () => {
           </Link>
         </>
       )}
+
+      {/* Delete Modal */}
+      <dialog id="delete_modal" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">{`Do you want to delete this calendar: ${getSelectedCalTitle()}?`}</h3>
+          <p className="py-4">This action can't be reversed.</p>
+          <div className="modal-action">
+            <form method="dialog" className="flex gap-5">
+              <button className="btn btn-error" onClick={deleteCalendar}>
+                Delete
+              </button>
+              <button className="btn btn-outline">Cancel</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </main>
   );
 };
--- frontend/src/components/CalendarCard.tsx
+++ frontend/src/components/CalendarCard.tsx
@@ -4,27 +4,35 @@ import { FaEdit } from 'react-icons/fa';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 
 type CalendarCardProps = {
+  id: string;
   title: string;
   imageUrl: string;
+  handleAction: (id: string, action: string) => void;
 };
 
-const CalendarCard: React.FC<CalendarCardProps> = ({ title, imageUrl }) => {
+const CalendarCard: React.FC<CalendarCardProps> = ({ id, title, imageUrl, handleAction }) => {
+  const handleClick = (action: 'show' | 'share' | 'edit' | 'delete') => {
+    handleAction(id, action);
+  };
+
   return (
     <div className="card w-80 bg-[#e2e8f0] shadow-xl px-6 py-4 grid grid-rows-[1.5fr_1fr]">
       <figure>
-        <Image src={imageUrl} alt={title} width={320} height={320} className="rounded-xl object-cover object-center w-full h-full" />
+        <button className="w-full h-full" onClick={() => handleClick('show')}>
+          <Image src={imageUrl} alt={title} width={320} height={320} className="rounded-xl object-cover object-center w-full h-full" />
+        </button>
       </figure>
       <div className="card-body items-center text-center">
         <h2 className="card-title text-lg pb-4">{title}</h2>
 
         <div className="card-actions flex gap-5">
-          <button className="btn btn-square btn-outline">
+          <button className="btn btn-square btn-outline" onClick={() => handleClick('share')}>
             <IoShareSocial fontSize={30} />
           </button>
-          <button className="btn btn-square btn-outline">
+          <button className="btn btn-square btn-outline" onClick={() => handleClick('edit')}>
             <FaEdit fontSize={30} />
           </button>
-          <button className="btn btn-square btn-outline" style={{ background: 'pink' }}>
+          <button className="btn btn-square btn-outline" style={{ background: 'pink' }} onClick={() => handleClick('delete')}>
             <RiDeleteBin6Line fontSize={30} />
           </button>
         </div>
